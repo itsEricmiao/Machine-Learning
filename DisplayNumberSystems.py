14e5fa12e7763b2995852dad4ca088c048301a59
Eric Miao <hongyum@smu.edu>
2019-01-30 19:01:55 -0600

    Add decimal to other number system converter function

--- DisplayNumberSystems.py
+++ DisplayNumberSystems.py
@@ -11,12 +11,33 @@
 # convertToHex
 # convertToDec
 
-
+# Step 1: Converting decimal to other number systems
 def convertToBinary(decimal): # the function will take a decimal input as parameter and return the binary number
     if decimal > 1:
         convertToBinary(decimal // 2)   # recursion if decimal is larger than 1
     print(decimal % 2, end = '') # Dividing the number successively by 2 and printing the remainder in reverse order.
 
+
+def convertToTernary(decimal): # the function will take a decimal input as parameter and return the binary number
+    if decimal > 3:
+        convertToTernary(decimal // 3)   # recursion if decimal is larger than 1
+    print(decimal % 3, end = '') # Dividing the number successively by 2 and printing the remainder in reverse order.
+
+
+def convertToQuaternary(decimal): # the function will take a decimal input as parameter and return the binary number
+    if decimal > 4:
+        convertToQuaternary(decimal // 4)   # recursion if decimal is larger than 1
+    print(decimal % 4, end = '') # Dividing the number successively by 2 and printing the remainder in reverse order.
+
+
+def convertToOctal(decimal): # the function will take a decimal input as parameter and return the binary number
+    if decimal > 8:
+        convertToOctal(decimal // 8)   # recursion if decimal is larger than 1
+    print(decimal % 8, end = '') # Dividing the number successively by 2 and printing the remainder in reverse order.
+
+
+
+# Step 2: Converting binary to other number systems
 def convertToDec(binary):
     power = 0;
     while binary > 1:
@@ -26,16 +47,15 @@ def convertToDec(binary):
         power = power + 1
 
 
-
-def get_digit(num):
-    if num < 10:
+def get_digit(num): # this is a function that can get each digit of the number from right to left
+    if num < 10: # if the digit is less than 10, we just return the number since it is only 1 digit
         return num
     else:
         get_digit(num // 10)
-        return (num % 10)
+        return num % 10
+
 
 x = 1100
-convertToDec(x)
 
-
+convertToQuaternary(350)
 
