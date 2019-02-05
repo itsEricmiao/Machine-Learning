5695f7d924afaf270a9af55efda62fef83579d88
Eric Miao <hongyum@smu.edu>
2019-02-05 17:25:20 -0600

    init PA02

--- wordsFrequency.py
+++ wordsFrequency.py
@@ -2,3 +2,20 @@
 # PA02: Words Frequency
 # Instructor: Nasser Jan
 # Name: Eric Miao
+
+items = ["Liam", "Mason", "William", "Noah", "William", "James", "Sophia", "Logan", "Benjamin", "Mason", "Elijah",
+         "Oliver", "Jacob", "Emma", "Olivia", "Ava", "William", "Isabella", "Oliver", "Sophia", "Mia", "Charlotte",
+         "Amelia", "William", "Evelyn", "Abigail", "Olivia", "Ava", "Mason", "Isabella", "Noah", "William", "James",
+         "Olivia", "Amelia", "Oliver", "William"]
+
+newList = []
+for i in range(len(items)-1):
+    j = i + 1
+    for j in range(len(items)):
+        if(items[i] == items[j]):
+            if items[i] in newList:
+                pass # do nothing
+            else:
+                newList.append(items[i])
+print(newList)
+
