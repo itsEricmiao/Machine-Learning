c1f2788ba2951db65831c578cac24de049a33e46
Eric Miao <hongyum@smu.edu>
2019-02-08 19:06:57 -0600

    add new funcs

--- wordsFrequency.py
+++ wordsFrequency.py
@@ -2,7 +2,7 @@
 # PA02: Words Frequency
 # Instructor: Nasser Jan
 # Name: Eric Miao
-
+import operator
 items = ["Liam", "Mason", "William", "Noah", "William", "James", "Sophia", "Logan", "Benjamin", "Mason", "Elijah",
          "Oliver", "Jacob", "Emma", "Olivia", "Ava", "William", "Isabella", "Oliver", "Sophia", "Mia", "Charlotte",
          "Amelia", "William", "Evelyn", "Abigail", "Olivia", "Ava", "Mason", "Isabella", "Noah", "William", "James",
@@ -10,7 +10,6 @@ items = ["Liam", "Mason", "William", "Noah", "William", "James", "Sophia", "Loga
 
 
 def word_freq(list):
-
     counts = dict()
     for i in list:
         counts[i] = counts.get(i, 0) + 1
@@ -18,10 +17,29 @@ def word_freq(list):
 
 
 def asc_word_freq(dict): # accepts dictionary of words’ counts
-    pass
+    sorted_d = sorted(dict.items(), key=operator.itemgetter(0))
+    return sorted_d
+
+def desc_word_freq(dict):  # accepts dictionary of words’ counts
+    sorted_d = sorted(dict.items(), key=operator.itemgetter(0), reverse=True)
+    return sorted_d
     # returns a dictionary of word counts in ascending order to the main method.
+
+def size_dict(d, n):
+    temp = dict()
+    size = len(d)
+    print(size)
+    if( n > size):
+        return d
+    elif(n < 0):
+        return temp
+    else:
+        
+
+
 
 # main function starts here
 print(word_freq(items))
-
-
+print(asc_word_freq(word_freq(items)))
+print(desc_word_freq(word_freq(items)))
+print(size_dict(word_freq(items), 5))
