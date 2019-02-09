abcf037b71f00225e818625147b70a5e436c9119
Eric Miao <hongyum@smu.edu>
2019-02-09 16:54:59 -0600

    complete PA02

--- wordsFrequency.py
+++ wordsFrequency.py
@@ -3,14 +3,6 @@
 # Instructor: Nasser Jan
 # Name: Eric Miao
 import operator
-from itertools import islice
-
-
-items = ["Liam", "Mason", "William", "Noah", "William", "James", "Sophia", "Logan", "Benjamin", "Mason", "Elijah",
-         "Oliver", "Jacob", "Emma", "Olivia", "Ava", "William", "Isabella", "Oliver", "Sophia", "Mia", "Charlotte",
-         "Amelia", "William", "Evelyn", "Abigail", "Olivia", "Ava", "Mason", "Isabella", "Noah", "William", "James",
-         "Olivia", "Amelia", "Oliver", "William"]
-
 
 def word_freq(list):
     counts = dict()
@@ -22,27 +14,31 @@ def word_freq(list):
 def asc_word_freq(dict): # accepts dictionary of words’ counts
     sorted_d = sorted(dict.items(), key=operator.itemgetter(1))
     # NOTE: if we want to sort the dict by key, we simply change the val from 1 to 0
-
     return sorted_d
 
 def desc_word_freq(dict):  # accepts dictionary of words’ counts
-    sorted_d = sorted(dict.items(), key=operator.itemgetter(1), reverse=True)
-    return sorted_d
+    #sorted_d = sorted(dict.items(), key=operator.itemgetter(1), reverse=True)
+    sorted_d = sorted(dict.items(), key=lambda t: t[1], reverse=True)
+    return (sorted_d)
     # returns a dictionary of word counts in ascending order to the main method.
 
 def size_dict(d, n):
     temp = dict()
     size = len(d)
-    if( n > size):
+    if( n > size): # if the n is out of range, we return the whole dict
         return d
-    elif(n < 0):
+    elif(n < 0): # if the n is out of range, we return an empty dict
         return temp
-    else:
-        target = {}
+    else: # normal case: 0 < n < size
         new_d = desc_word_freq(d)
         target = new_d[:n]
         return target
 
+
+items = ["Liam", "Mason", "William", "Noah", "William", "James", "Sophia", "Logan", "Benjamin", "Mason", "Elijah",
+         "Oliver", "Jacob", "Emma", "Olivia", "Ava", "William", "Isabella", "Oliver", "Sophia", "Mia", "Charlotte",
+         "Amelia", "William", "Evelyn", "Abigail", "Olivia", "Ava", "Mason", "Isabella", "Noah", "William", "James",
+         "Olivia", "Amelia", "Oliver", "William"]
 
 # main function starts here
 print("1. Name Frequency: ")
@@ -58,5 +54,5 @@ print(desc_word_freq(word_freq(items)))
 print('')
 
 print("4. Returns a subset of dictionary with n most frequent words. ")
-print(size_dict(word_freq(items), 5))
+print(size_dict(word_freq(items), -1))
 print('')
