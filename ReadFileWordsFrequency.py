fcddc464277cfe74ccab9bd0de2538ce0234689d
Eric Miao <hongyum@smu.edu>
2019-02-14 17:13:12 -0600

    add func 2 and 3 for PA03

--- ReadFileWordsFrequency.py
+++ ReadFileWordsFrequency.py
@@ -3,24 +3,31 @@
 # # Instructor: Nasser Jan
 # # Name: Eric Miao
 
-
 import re
 def readfile(path):
-    days_file = open(path, 'r')  # input the file
-    mystr = days_file.read()
-    wordList = re.sub("[^\w]", " ", mystr).split()
+    days_file = open(path, 'r')  # Input the file
+    mystr = days_file.read() # Read in the file
+    wordList = re.sub("[^\w]", " ", mystr).split() # Split string to the words
     return wordList
 
 def word_freq(list):
-    counts = dict()
-    for i in list:
+    counts = dict() # Initilize a dictionary
+    for i in list: # Loop through the list and add instances if found in the dictionary
         counts[i] = counts.get(i, 0) + 1
     return counts # The word_freq() returns a dictionary of word counts to the main method.
 
+def word_no_duplicate (items):
+    return  sorted(list(set(items))) # return a sorted list with non-duplicates
 
 #main function starts from here:
 path = '/Users/ericmiao/Desktop/Programming/Python/smu.txt' # getting the path to the smu.txt file
-a = readfile(path)
-b = word_freq(a)
-print(b)
+wordList = readfile(path)
+dict = word_freq(wordList)
+print("1. Dictionary is: ")
+print(dict)
+print()
+
+print("2. List that contains non-duplicates only is: ")
+print(word_no_duplicate(wordList))
+
 
