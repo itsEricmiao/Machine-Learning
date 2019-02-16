6360409702c43598b360df0719567ae1444738e8
Eric Miao <hongyum@smu.edu>
2019-02-16 13:25:30 -0600

    Commit PA03

--- ReadFileWordsFrequency.py
+++ ReadFileWordsFrequency.py
@@ -1,12 +1,13 @@
 # CSE-3342 Spring 2019
-# # PA03: Read File Words Frequency
-# # Instructor: Nasser Jan
-# # Name: Eric Miao
+# PA03: Read File Words Frequency
+# Instructor: Nasser Jan
+# Name: Eric Miao
 import re
 def readfile(path):
     days_file = open(path, 'r')  # Input the file
     mystr = days_file.read() # Read in the file
     wordList = re.sub("[^\w]", " ", mystr).split() # Split string to the words
+    days_file.close()
     return wordList
 
 def word_freq(list):
@@ -16,22 +17,28 @@ def word_freq(list):
     return counts # The word_freq() returns a dictionary of word counts to the main method.
 
 def word_no_duplicate (items):
-    return  set(sorted(set(items)))# return a sorted list with non-duplicates
+    return  set(sorted(set(items)))  # return a sorted set with non-duplicates
 
 def dic_no_duplicate (dict):
-    return  set(sorted(set(dict)))
+    return  set(sorted(set(dict))) # return a sorted set with non-duplicates
 
 #main function starts from here:
 path = '/Users/ericmiao/Desktop/Programming/Python/smu.txt' # getting the path to the smu.txt file
 wordList = readfile(path)
 dict = word_freq(wordList)
-print("1. Dictionary is: ")
+
+print("1. List is: ")
+print(wordList)
+print()
+
+print("2. Dictionary is: ")
 print(dict)
 print()
 
-print("2. Input list and return set that contains non-duplicates only: ")
+print("3. Input list and return set that contains non-duplicates only: ")
 print(word_no_duplicate(wordList))
 print()
 
-print("3. Input dictionary and return set that contains non-duplicates only: ")
+print("4. Input dictionary and return set that contains non-duplicates only: ")
 print(dic_no_duplicate(dict))
+
