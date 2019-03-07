5985d81fbfe9fcf27f8c6a70a9461f172ca736d0
Eric Miao <hongyum@smu.edu>
2019-03-06 21:26:53 -0600

    Update code

--- guess.pl
+++ guess.pl
@@ -3,28 +3,32 @@ $input = <STDIN>;
 #	first name and last name: John Quincy Adams
 
 #	The regular expression should determine the input data is male or female: 
-	
+	$male = 'male';
+	$female = 'female';
+	if ($input =~ $male )
+	{
+			print "It is an gender: $input";
+	}
+	if ($input =~ $female )
+	{
+			print "It is an gender: $input";
+	}
 #	The regular expression should check the input data is street address: 5454 Amesbury Dr
 	
 #	The regular expression matches the city, state and zip code: Dallas, TX 75206
 
 #	The regular expression should match the email address 
-	if ($input == ~/(@)/ )
+	if ($input =~ /@/ )
 	{
 		print "It is an email address: $input";
-	}else {
-		print "It is NOT an email address: $input";
 	}
 #	The regular expression should match the phone number
 
 #	The regular expression for currency
-	
-#	The regular expression for Monetary Input: $55.00 or $0.75, etc 
-	if ($input == ~/$/ )
+
+	if ($input =~ /($)/ )
 	{
 		print "It is a Currency: $input";
-	}else {
-		print "It is NOT an Currency: $input";
 	}
 #	The regular expression for debit or credit card number
 	
