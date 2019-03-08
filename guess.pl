d5e81d55ecec6c87448614587441619821cfecfd
Eric Miao <hongyum@smu.edu>
2019-03-07 18:09:46 -0600

    Commit PA05

--- guess.pl
+++ guess.pl
@@ -1,108 +1,131 @@
-#!/usr/bin/perl
-$input = <STDIN>;
+#Eric Miao
+#CSE-3342
+#Programming Assignment 5: Perl with Regex
 
+#!/usr/bin/perl
+$male = 'male';
+$female = 'female';
+while ($input = <STDIN>) {
+	$input_lowercase = lc $input;
 #	first name and last name: John Quincy Adams
 	if($input =~ /^[A-Z][a-z]+\s[A-Z][a-z]+$/g)
 	{
 		print "Input is: $input";
 		print "It looks like a first name and last name.";
 	}
-	
-	if($input =~ /^[A-Z][a-z]+\s+[A-Z][a-z]+\s+[A-Z][a-z]+$/g)
+	elsif($input =~ /^[A-Z][a-z]+\s+[A-Z][a-z]+\s+[A-Z][a-z]+$/g)
+	{
+		print "Input is: $input";
+		print "It looks like a first name, middle name, and last name.";
+	}
+	elsif($input =~ /^[A-Z][a-z]+\s+[A-Z]+\s+[A-Z][a-z]+$/g)
 	{
 		print "Input is: $input";
 		print "It looks like a first name, middle name, and last name.";
 	}
 	
 #	The regular expression should determine the input data is male or female: 
-	$male = 'male';
-	$female = 'female';
-	if ($input =~ $male || $input =~ $female )
+	elsif ($input_lowercase =~ $male)
 	{
 		print "Input is: $input";
-		print "It looks like a gender number.";
+		print "It looks like a gender.";
+	}
+	
+	elsif ($input_lowercase =~ $female)
+	{
+		print "Input is: $input";
+		print "It looks like a gender.";
 	}
 
 #	The regular expression should check the input data is street address: 5454 Amesbury Dr
-	if($input =~ /([0-9]+)(\s)([A-Z]{1})([a-z]+)(\s)([A-Z]{1})([a-z]+)/)
+	elsif($input =~ /([0-9]+)(\s)([A-Z]{1})([a-z]+)(\s)([A-Z]{1})([a-z]+)/)
 	{
 		print "Input is: $input";
 		print "It looks like a street address.";
 	}
 	
 #	The regular expression matches the city, state and zip code: Dallas, TX 75206
-	if($input =~ /([A-Z]{1})([a-z]+)(,)(\s)([A-Z]{2})(\s)([0-9]+)/g)
+	elsif($input =~ /([A-Z]{1})([a-z]+)(,)(\s)([A-Z]{2})(\s)([0-9]+)/g)
 	{
 		print "Input is: $input";
 		print "It looks like a city, state and zip code.";
 	}
 	
 #	The regular expression should match the email address 
-	if ($input =~ /@/ )
+	elsif ($input =~ /@/ )
 	{
 		print "Input is: $input";
 		print "It looks like an email address.";
 	}
 	
 #	The regular expression should match the phone number
-	if ($input =~ /[0-9]{3}-[0-9]{3}-[0-9]{4}/ )
+	elsif ($input =~ /[0-9]{3}-[0-9]{3}-[0-9]{4}/ )
 	{
 		print "Input is: $input";
 		print "It looks like a phone number.";
 	}
 
 #	The regular expression for currency
-	if ($input =~ /\$/ )
+	elsif ($input =~ /\$/ )
 	{
 		print "Input is: $input";
 		print "It looks like a currency.";
 	}
 	
 #	The regular expression for debit or credit card number
-	if ($input =~ /[0-9]{4}-[0-9]{4}-[0-9]{4}/ )
+	elsif ($input =~ /[0-9]{4}-[0-9]{4}-[0-9]{4}/ )
 	{
 		print "Input is: $input";
 		print "It looks like a credit card number.";
 	}
 	
 #	The regular expression for date
-	if ($input =~ /[0-9]{4}(-)[0-9]{2}-[0-9]{2}/ )
+	elsif ($input =~ /[0-9]{4}(-)[0-9]{2}-[0-9]{2}/ )
 	{
 		print "Input is: $input";
 		print "It looks like an date.";
 	}
-	if ($input =~ /[0-9]{2}(-)[0-9]{2}-[0-9]{4}/ )
+	elsif ($input =~ /[0-9]{2}(-)[0-9]{2}-[0-9]{4}/ )
 	{
 		print "Input is: $input";
 		print "It looks like an date.";
 	}
-	if ($input =~ /[0-9]{2}.[0-9]{2}.[0-9]{4}/ )
+	elsif ($input =~ /[0-9]{2}.[0-9]{2}.[0-9]{4}/ )
 	{
 		print "Input is: $input";
 		print "It looks like an date.";
 	}
-	if ($input =~ /[0-9]{4}.[0-9]{2}.[0-9]{2}/ )
+	elsif ($input =~ /[0-9]{4}.[0-9]{2}.[0-9]{2}/ )
 	{
 		print "Input is: $input";
 		print "It looks like an date.";
 	}
 	
 #	The regular expression for time  
-	if ($input =~ /[0-9]{2}:[0-9]{2}/ )
+	elsif ($input =~ /[0-9]{2}:[0-9]{2}/ )
 	{
 		print "Input is: $input";
 		print "It looks like a time.";
 	}
 	
 #	The regular expression for URL
-#	if ($input =)
-#		{
-#			print "Input is: $input";
-#			print "It looks like a URL.";
-#		}
+	elsif ($input_lowercase =~ /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ )
+	{
+			print "Input is: $input";
+			print "It looks like an URL.";
+	}
+		
 #	The regular expression for IP Address
-	if ($input =~ /[0-9]{3}(.)[0-9]{3}(.)[0-9]{3}(.)[0-9]{1}/ )
+	elsif ($input =~ /[0-9]{3}(.)[0-9]{3}(.)[0-9]{3}(.)[0-9]{1}/ )
 	{
 		print "Input is: $input";
 		print "It looks like an IP address.";
+	}else {
+		{
+			print "I don't know what it is... $input";
+		}
 	}
+	print "\n";
+	print "\n";
+
+}
